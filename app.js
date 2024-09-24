ad57f8d5fa8e5bad4adce19da130a321da6bc780
dikshitha-jella <Dikshithajella2004@gmail.com>
2024-09-25 02:13:52 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
-// app.js
 const express = require('express');
 const path = require('path');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Serve static files (CSS) from the root directory
-app.use(express.static(__dirname));
+// Serve static files (CSS, JS, images) from the root directory
+app.use(express.static(path.join(__dirname)));
 
 // Serve the index.html file when the root route is accessed
 app.get('/', (req, res) => {
@@ -16,5 +15,4 @@ app.get('/', (req, res) => {
 // Start the server
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
-
 });
